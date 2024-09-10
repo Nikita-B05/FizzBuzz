03be4204d9ae4e25db7118689f67f96791c1d55d
Nikita-B05 <108193728+Nikita-B05@users.noreply.github.com>
2024-09-09 20:05:35 -0400

    refactored to extract helper method and added README.md

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,14 +4,14 @@
 class FizzBuzz {
 
     public static void main(String[] args) {
-        int i = 0;
-        while (i < 99) {
+        int i = 1;
+        while (i < 100) {
             i = doFizzBuzz(i);
         }
     }
 
     private static int doFizzBuzz(int i) {
-        i++;
+
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
@@ -34,6 +34,7 @@ class FizzBuzz {
             System.out.println(i);
 
         }
+        i++;
         return i;
     }
 }
