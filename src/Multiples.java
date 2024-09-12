a6a0f46aee6353fae701d57fa6f2a71fc34ba906
Nikita-B05 <108193728+Nikita-B05@users.noreply.github.com>
2024-09-12 09:01:01 -0400

    Fixed the loop to be below 1000, instead of <= 1000

--- src/Multiples.java
+++ src/Multiples.java
@@ -2,7 +2,7 @@ public class Multiples {
     public static void main(String[] args) {
         int i = 1;
         int counter = 0;
-        while (i <= 1000) {
+        while (i < 1000) {
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
 
